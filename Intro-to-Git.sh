31cedeff313ec480c1a3ea87dd5990ac706d315e
Paschal Ugwu <106428248+paschalugwu@users.noreply.github.com>
2023-02-17 15:36:46 +0100

    Create Intro-to-Git.sh

---
+++ Intro-to-Git.sh
@@ -0,0 +1,29 @@
+#1 To check the version
+git --version
+
+#2 To get help online
+git help confiq
+
+#3 To get help on the terminal
+git help
+
+#4 To initialize a directory
+git init
+
+#5 To check the status of an action
+git status
+
+#6 To add a named file to the current directory
+git add exam.txt
+
+#7 To add the new file to all directories
+git commit -m "committing a text file"
+
+#8 To link our git to github account
+git config --global user.username paschalugwu
+
+#9 To link the remote and local repository
+git remote add origin https://github.com/paschalugwu/test_demo.git
+
+#10 To push local file into the repository
+git push origin master
